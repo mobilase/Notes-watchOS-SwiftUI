1bbfabd32a5444a3eab89328b66951020babf758
Sergey Lavrov <lavrov-sergey@yandex.ru>
2021-03-08 01:43:14 +0300

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Сергей Лавров on 08.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
