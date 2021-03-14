e597c36bf927c08883117a6519217bd6dffbc370
Sergey Lavrov <lavrov-sergey@yandex.ru>
2021-03-14 19:49:45 +0300

    Edit text on click and not need button

--- NotesSwiftUI WatchKit Extension/View/DetailView.swift
+++ NotesSwiftUI WatchKit Extension/View/DetailView.swift
@@ -13,16 +13,15 @@ struct DetailView: View {
     
     var body: some View {
         VStack(alignment: .leading){
-            Text(note.text ?? "").font(.body);
-            NavigationLink(destination:EditView(note: note), label: {
-                Text("Изменить")
-            }).background(Color.green).cornerRadius(5.0)
+            NavigationLink(destination: EditView(note: note)) {
+                ScrollView {
+                    Text(note.text ?? "")
+                        .font(.body)
+                        .lineLimit(nil)
+                        .fixedSize(horizontal: false, vertical: true)
+                }
+            }
         }
-        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,
-               maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
-               minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,
-               maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
-               alignment: .topLeading)
         .navigationTitle(note.title ?? "")
     }
 }
